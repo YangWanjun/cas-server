fc407494ff0f3d9a2d3ccbeb21a17e98d7457ef7
YangWanjun <ywjsailor@gmail.com>
2021-11-12 23:28:59 +0900

    login page

--- cas/settings.py
+++ cas/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'cas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,7 +134,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = STATIC_ROOT = '/static/'
+STATICFILES_DIRS = (
+    BASE_DIR / "static",
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
